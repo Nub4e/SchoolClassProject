664b7fa9c8a0cc57b90ca4cc10baff0bdb608f34
thealmostnooBBg <thealmostnooBBg@abv.bg>
2019-02-27 23:38:52 +0200

    There was a mistake last time so I'm doing it again.

--- ClassBookDatabase.sql
+++ ClassBookDatabase.sql
@@ -11,10 +11,78 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+-- Дъмп на структурата на БД classbook
+CREATE DATABASE IF NOT EXISTS `classbook` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `classbook`;
+
+-- Дъмп структура за таблица classbook.classes
+CREATE TABLE IF NOT EXISTS `classes` (
+  `class_id` int(11) NOT NULL AUTO_INCREMENT,
+  `grade` int(2) NOT NULL,
+  `letter` varchar(2) NOT NULL,
+  `head_teacher_id` int(11) NOT NULL,
+  PRIMARY KEY (`class_id`),
+  UNIQUE KEY `head_teacher_id` (`head_teacher_id`),
+  CONSTRAINT `fk_classes_teachers` FOREIGN KEY (`head_teacher_id`) REFERENCES `teachers` (`teacher_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 -- Изнасянето на данните беше деселектирано.
+-- Дъмп структура за таблица classbook.marks
+CREATE TABLE IF NOT EXISTS `marks` (
+  `mark_id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` enum('слаб','среден','добър','много добър','отличен') NOT NULL,
+  `number` double(3,2) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  PRIMARY KEY (`mark_id`),
+  KEY `fk_marks_subjects` (`subject_id`),
+  KEY `fk_marks_students` (`student_id`),
+  CONSTRAINT `fk_marks_students` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`),
+  CONSTRAINT `fk_marks_subjects` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`subject_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 -- Изнасянето на данните беше деселектирано.
+-- Дъмп структура за таблица classbook.students
+CREATE TABLE IF NOT EXISTS `students` (
+  `student_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(20) NOT NULL,
+  `middle_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `phone_number` varchar(12) NOT NULL,
+  `birthdate` date NOT NULL,
+  `class_id` int(11) NOT NULL,
+  PRIMARY KEY (`student_id`),
+  KEY `fk_students_classes` (`class_id`),
+  CONSTRAINT `fk_students_classes` FOREIGN KEY (`class_id`) REFERENCES `classes` (`class_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 -- Изнасянето на данните беше деселектирано.
+-- Дъмп структура за таблица classbook.subjects
+CREATE TABLE IF NOT EXISTS `subjects` (
+  `subject_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`subject_id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 -- Изнасянето на данните беше деселектирано.
+-- Дъмп структура за таблица classbook.teachers
+CREATE TABLE IF NOT EXISTS `teachers` (
+  `teacher_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(20) NOT NULL,
+  `middle_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `phone_number` varchar(12) NOT NULL,
+  `birthdate` date NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  PRIMARY KEY (`teacher_id`),
+  KEY `fk_teachers_subjects` (`subject_id`),
+  CONSTRAINT `fk_teachers_subjects` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`subject_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 -- Изнасянето на данните беше деселектирано.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
