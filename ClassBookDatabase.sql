9ca8d04df73c9f13ee112d8df1c32c25081f09ad
thealmostnooBBg <thealmostnooBBg@abv.bg>
2019-02-27 23:33:59 +0200

    Just a database prototype.

---
+++ ClassBookDatabase.sql
@@ -0,0 +1,21 @@
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия на сървъра:            8.0.13 - MySQL Community Server - GPL
+-- ОС на сървъра:                Win64
+-- HeidiSQL Версия:              10.1.0.5464
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Изнасянето на данните беше деселектирано.
+-- Изнасянето на данните беше деселектирано.
+-- Изнасянето на данните беше деселектирано.
+-- Изнасянето на данните беше деселектирано.
+-- Изнасянето на данните беше деселектирано.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
