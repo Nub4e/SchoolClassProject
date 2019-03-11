a75fdd368d02356a50ef5f917e0cf7678d30f8c9
Valentin Iliev <35625314+ValentinIliev5@users.noreply.github.com>
2019-03-11 10:56:12 +0200

    Update Mark.cs

--- ConsoleApplication7/ConsoleApplication7/Mark.cs
+++ ConsoleApplication7/ConsoleApplication7/Mark.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//TEST rewritten ToString(), mark
 namespace ConsoleApplication7
 {
     public class Mark
